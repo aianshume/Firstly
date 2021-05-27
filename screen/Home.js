1c33815eac6f864c321cd877ee406fb671eb7f9c
Anshumeena1947 <anshumeena1947@gmail.com>
2021-05-27 08:58:14 +0530

    do some chages that not working

--- screen/Home.js
+++ screen/Home.js
@@ -1,30 +1,71 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView } from 'react-native';
-// import LoadingScr from './compo/LoadingScr'
+import { View, Text, StyleSheet, TouchableOpacity, Image, ScrollView, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { Ionicons } from '@expo/vector-icons';
-import faker from 'faker';
+import faker, { datatype } from 'faker';
 import StoryAvator from './compo/StoryAvator';
 import NewBigBox from './compo/NewsBigBox'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import firebase from '../firebaseConfig';
 import LoadingPage from './compo/LoadingScr'
 
+var setArticleToLoad = 0;
+var dataToPrint = [];
+var isUserFollowing = false;
+var userFollowing = [];
 
 export default function HomeScreen({ navigation }) {
-    const [isLoaded, setIsLoaded] = React.useState(false)
+    const [isLoaded, setIsLoaded] = React.useState(false);
     const db = firebase.firestore();
     var un = firebase.auth().currentUser.email;
+    const renderItem = ({ item }) => (
+        <NewBigBox
+            username={item.user.username}
+            fullArticle={item.post.Article}
+            catagory={item.post.Category}
+            newsImage={item.post.Image}
+            newsHeading={item.post.Title}
+            channelNews={item.user.name}
+            avatorImg={item.user.avator}
+            nav={navigation}
+        />
+    );
 
     React.useEffect(() => {
         const loadData = () => {
             db.collection("Accounts").doc(un.substring(0, un.search('@gmail.com'))).get().then(async (doc) => {
                 if (doc.exists) {
+                    userFollowing = doc.data().following;
+                    let dataToStore = JSON.stringify(doc.data());
+                    await AsyncStorage.setItem("@userProfile", dataToStore);
+                    await console.log('data stored')
                     try {
-                        let value = JSON.stringify(doc.data())
-                        await AsyncStorage.setItem('@userProfile', value).then(() => console.log('data saved in asyncStorage'))
-                        setIsLoaded(true)
+                        console.log(userFollowing)
+                        let dump = Object.getOwnPropertyNames(userFollowing);
+                        for (var i = 0; i < dump.length; i++) {
+                            console.log(userFollowing[i]);
+                            await db.collection('Accounts').doc(userFollowing[i]).get().then(async (doc) => {
+                                if (doc.exists) {
+                                    let articles = doc.data().Articles;
+                                    let articleToLoad = [articles, articles - 1];
+                                    let userToLoad = userFollowing[i];
+                                    for (var j = 0; j < 2; j++) {
+                                        await db.collection('Accounts').doc(userToLoad).collection('Posts').doc(j.toString()).get().then(async (doc) => {
+                                            if (doc.exists) {
+                                                let postData = doc.data();
+                                                
+                                                
+                                                setIsLoaded(true)
+                                            } else {
+                                                console.log('doc not found , user: ' + userToLoad + ' and posts ' + j);
+                                                setIsLoaded(true)
+                                            }
+                                        })
+                                    }
+                                }
+                            })
+                        }
                     } catch (e) {
                         console.log('error: ' + e)
                     }
@@ -43,7 +84,7 @@ export default function HomeScreen({ navigation }) {
         return (<LoadingPage />)
     } else if (isLoaded == true) {
         return (
-            <View style={{ backgroundColor: '#f1f1f1' }}>
+            <View>
                 <SafeAreaView>
                     <StatusBar style='dark' />
                     <View style={styles.HeaderPart}>
@@ -65,51 +106,15 @@ export default function HomeScreen({ navigation }) {
                                 <StoryAvator imageUrl={faker.image.avatar()} />
                             </ScrollView>
                         </View>
-                        {/* news card view */}
-                        <View style={styles.newsCardBoxList}>
-                            <NewBigBox
-                                username={faker.name.firstName()}
-                                avatorImg={faker.image.avatar()}
-                                channelNews={faker.company.companyName()}
-                                newsImage={faker.image.imageUrl()}
-                                newsHeading={faker.lorem.lines(2)}
-                                catagory={faker.commerce.department()}
-                                nav={navigation}
-                            />
-                            <NewBigBox
-                                username={faker.name.firstName()}
-                                avatorImg={faker.image.avatar()}
-                                channelNews={faker.company.companyName()}
-                                newsImage={faker.image.imageUrl()}
-                                newsHeading={faker.lorem.lines(2)}
-                                catagory={faker.commerce.department()}
-                            />
-                            <NewBigBox
-                                username={faker.name.firstName()}
-                                avatorImg={faker.image.avatar()}
-                                channelNews={faker.company.companyName()}
-                                newsImage={faker.image.imageUrl()}
-                                newsHeading={faker.lorem.lines(2)}
-                                catagory={faker.commerce.department()}
-                            />
-                            <NewBigBox
-                                username={faker.name.firstName()}
-                                avatorImg={faker.image.avatar()}
-                                channelNews={faker.company.companyName()}
-                                newsImage={faker.image.imageUrl()}
-                                newsHeading={faker.lorem.lines(2)}
-                                catagory={faker.commerce.department()}
-                            />
-                            <NewBigBox
-                                username={faker.name.firstName()}
-                                avatorImg={faker.image.avatar()}
-                                channelNews={faker.company.companyName()}
-                                newsImage={faker.image.imageUrl()}
-                                newsHeading={faker.lorem.lines(2)}
-                                catagory={faker.commerce.department()}
-                            />
-                        </View>
                     </ScrollView>
+                    {/* news card view */}
+                    <View style={styles.newsCardBoxList}>
+                        <FlatList
+                            data={dataToPrint}
+                            renderItem={renderItem}
+                            keyExtractor={item => dataToPrint.indexOf(item) + 1}
+                        />
+                    </View>
                 </SafeAreaView>
             </View>
         )
@@ -139,5 +144,5 @@ const styles = StyleSheet.create({
         marginLeft: 10,
         marginRight: 10,
         marginBottom: 80,
-    }
+    },
 })
