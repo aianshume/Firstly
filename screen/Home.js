1a02ba65a04d1730f822a40b165f70c427f6331a
Anshu <anshumeena1947@gmail.com>
2021-06-05 17:16:46 +0530

    everything work

--- screen/Home.js
+++ screen/Home.js
@@ -14,8 +14,6 @@ var isUserFollowing = false;
 var userFollowing = [];
 var userTempData = [];
 
-//checking that sublime text merge
-
 export default function HomeScreen({ navigation }) {
     const [isLoaded, setIsLoaded] = React.useState(false);
     const [dataToPrint, setDataToPrint] = React.useState([]);
